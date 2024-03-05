38d5ca86306b46c4b2b276c58d35779eeb085bbf
Roberto Almeida <univil@gmail.com>
2024-03-05 12:25:28 -0600

    CampsitesDirectoryPage & CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
