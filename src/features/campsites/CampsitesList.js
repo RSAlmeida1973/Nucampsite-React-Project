a234af7fb6c05274a9c46f3ccfc0bf6701f8d5cd
Roberto Almeida <univil@gmail.com>
2024-03-31 11:12:15 -0500

    Clean up formatting

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -5,7 +5,6 @@ import { selectAllCampsites } from "./campsitesSlice";
 import Error from "../../components/Error";
 import Loading from "../../components/Loading";
 
-
 const CampsitesList = () => {
   const campsites = useSelector(selectAllCampsites);
 
